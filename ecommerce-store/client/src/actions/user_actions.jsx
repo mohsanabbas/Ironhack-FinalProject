795481762995f988987488a7d2d1082384fcbd27
Mohsan <mohsanabbas>
2018-12-08 22:59:18 -0200

    user logout is done

--- ecommerce-store/client/src/actions/user_actions.jsx
+++ ecommerce-store/client/src/actions/user_actions.jsx
@@ -3,7 +3,8 @@ import { USER_SERVER } from '../components/utils/gvar';
 import { 
     LOGIN_USER,
     REGISTER_USER,
-    AUTH_USER
+    AUTH_USER,
+    LOGOUT_USER
 } from './types';
 
 export function registerUser(dataToSubmit){
@@ -34,5 +35,16 @@ export function auth(){
         type: AUTH_USER,
         payload: request
     }
+}
+export function logoutUser(){
+
+    const request = axios.get(`${USER_SERVER}/logout`)
+    .then(response => response.data);
+
+    return {
+        type: LOGOUT_USER,
+        payload: request
+    }
+
 }
 
